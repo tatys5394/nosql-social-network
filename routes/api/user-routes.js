755db72c49b979646a1f84f3e7b61ff4ef050059
tatys5394 <123910162+tatys5394@users.noreply.github.com>
2023-04-18 19:57:35 -0400

    added user-routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,106 @@
+const router = require("express").Router()
+const { User } = require('../../models')
+
+// /api/users
+router
+    .route('/')
+    .get(async (req, res) => {
+        // get all users
+        try {
+            const users = await User.find().select('-__v')
+            res.json(users)
+        } catch (err) {
+            console.error(err)
+            res.status(500).json(err)
+        }
+    })
+    .post(async (req, res) => {
+        // create user
+        try {
+            const user = await User.create(req.body)
+            res.json(user)
+        } catch (err) {
+            console.error(err)
+            res.status(500).json(err)
+        }
+    })
+
+// /api/users/:id
+router
+    .route('/:id')
+    .get(async (req, res) => {
+        // get a single user
+        try {
+            const user = await User.findOne({_id: req.params.id})
+                .select('-__v')
+                .populate('thoughts')
+                .populate('friends')
+                
+            res.json(user)
+        } catch (err) {
+            console.error(err)
+            res.status(400).json(err)
+        }
+    })
+    .put(async (req, res) => {
+        // update user
+        try {
+            const user = await User.findOneAndUpdate(
+                {_id: req.params.id},
+                {$set: req.body},
+                {
+                    runValidators: true,
+                    new: true
+                }
+            )
+            res.json(user)
+        } catch (err) {
+            console.error(err)
+            res.status(500).json(err)
+        }
+    })
+    .delete(async (req, res) => {
+        // delete a single user
+        try {
+            const user = await User.findOneAndDelete({_id: req.params.id})
+            res.json(user)
+        } catch (err) {
+            console.error(err)
+            res.status(400).json(err)
+        }
+    })
+
+// /api/users/:userId/friends/:friendId
+
+router
+    .route('/:userId/friends/:friendId')
+    .post(async (req, res) => {
+        // add a friend
+        try {
+            const user = await User.findOneAndUpdate(
+                {_id: req.params.userId},
+                {$addToSet: {friends: req.params.friendId}},
+                {new: true}
+            )
+            res.json(user)
+        } catch (err) {
+            console.error(err)
+            res.status(400).json(err)
+        }
+    })
+    .delete(async (req, res) => {
+        // remove Friend
+        try {
+            const user = await User.findOneAndUpdate(
+                {_id: req.params.id},
+                {$pull: {friends: req.params.friendId}},
+                {new: true}
+            )
+            res.json(user)
+        } catch (err) {
+            console.error(err)
+            res.status(500).json(err)
+        }
+    })
+
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const router = require("express").Router()
+
+const apiRoutes = require('/api')
+
+router.use('/api', apiRoutes)
+
+router.use((req, res) => {
+    return res.send('404 not found')
+})
+
+module.exports = router
