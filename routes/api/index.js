3484b02c3eeaf386e7b0f0cf4b3511cc803ebfa4
tatys5394 <123910162+tatys5394@users.noreply.github.com>
2023-04-19 19:44:50 -0400

    fixed routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router()
 
 const userRoutes = require('./user-routes')
 
-router.use('./user-routes', userRoutes)
+router.use("/users",userRoutes)
 
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router()
 
 const apiRoutes = require('./api')
 
-router.use('./api', apiRoutes)
+router.use('/api', apiRoutes)
 
 router.use((req, res) => {
     return res.send('404 not found')
